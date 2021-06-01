1c865d28d7dfd69fcc43cab6a133030ed2acfa99
BrianCorbett <brian.corbett@revature.net>
2021-06-01 14:39:01 -0700

    committing my first hello world program

---
+++ my-first-hello-world/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace my_first_hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please enter your age:");
+            string age = Console.ReadLine();
+            Console.WriteLine($"Your name is {name} and you are {age} years old.");
+        }
+    }
+}
