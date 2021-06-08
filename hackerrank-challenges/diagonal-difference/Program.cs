9efc1451baeb2c2b1765be7ed7d6fb327c381793
BrianCorbett <brian.corbett@revature.net>
2021-06-07 21:52:58 -0700

    'diagonal difference challenge completed'

--- hackerrank-challenges/diagonal-difference/Program.cs
+++ hackerrank-challenges/diagonal-difference/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace diagonal_difference
 {
-    class Program
+    class Result
     {
-        static void Main(string[] args)
+
+        /*
+         * Complete the 'diagonalDifference' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+         */
+
+        public static int diagonalDifference(List<List<int>> arr)
         {
-            Console.WriteLine("Hello World!");
+            int n = arr.Count;
+
+            int PrimarySum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                PrimarySum += arr[i][i];
+            }
+
+            int SecondarySum = 0;
+            for (int i = (n - 1); i >= 0; i--)
+            {
+                SecondarySum += arr[i][(n - 1) - i];
+            }
+
+            return Math.Abs(PrimarySum - SecondarySum);
         }
+
     }
+
 }
